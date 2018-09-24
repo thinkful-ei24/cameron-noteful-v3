7e4a53edc0c064b936cefa04416440ae1521e276
Cameron Hatch <cameronlavigne92@gmail.com>
2018-09-24 13:46:23 -0500

    created User schema and model

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  fullname: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+userSchema.set('toObject', {
+  virtuals: true,
+  versionKey: false,
+  transform: (doc, result) => {
+    delete result._id;
+    delete result.__v;
+    delete result.password;
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
